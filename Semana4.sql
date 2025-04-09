ad1a08d944adb9502e17de0823116c2e2382d545
miguel-oliveira-silva <146590724+miguel-oliveira-silva@users.noreply.github.com>
2025-04-09 18:39:41 -0300

    Update Semana4.sql

--- Semana4.sql
+++ Semana4.sql
@@ -1,11 +1,12 @@
 -- Questão 1. Gere uma lista de todos os instrutores, mostrando sua ID, nome e número de seções que eles ministraram. 
 -- Não se esqueça de mostrar o número de seções como 0 para os instrutores que não ministraram qualquer seção. 
 -- Sua consulta deverá utilizar outer join e não deverá utilizar subconsultas escalares.
-SELECT INSTRUCTOR.ID, instructor.name, count(teaches.sec_id) AS num_sections from instructor left join
+SELECT INSTRUCTOR.ID, instructor.name, count(teaches.sec_id) AS num_sections from instructor left outer join
 teaches on instructor.id = teaches.id group BY INSTRUCTOR.ID, instructor.name; 
 
 --Questão 2. Escreva a mesma consulta do item anterior, mas usando uma subconsulta escalar, sem outer join.
-SELECT instructor.ID, instructor.name, (select count(teaches.sec_id) FROM teaches WHERE teaches.ID = instructor.ID) AS num_sections
+SELECT instructor.ID, instructor.name, (select count(teaches.sec_id) 
+FROM teaches WHERE teaches.ID = instructor.ID) AS num_sections
 FROM instructor;
 
 -- Questão 3. Gere a lista de todas as seções de curso oferecidas na primavera de 2010, junto com o nome dos instrutores ministrando a seção. 
@@ -26,4 +27,18 @@ FROM instructor;
 	   		 AND teaches.year = 2010
 	   	 ORDER BY 
    		 teaches.course_id Asc;
-    	 
+--Questão 4. Suponha que você tenha recebido uma relação grade_points (grade, points), que oferece uma conversão de conceitos (letras)
+na relação takes para notas numéricas; por exemplo, uma nota “A+” poderia ser especificada para corresponder a 4 pontos, 
+	um “A” para 3,7 pontos, e “A-” para 3,4, e “B+” para 3,1 pontos, e assim por diante. 
+--Os Pontos totais obtidos por um aluno para uma oferta de curso (section) são definidos como o número de créditos para o
+	curso multiplicado pelos pontos numéricos para a nota que o aluno recebeu.
+--Dada essa relação e o nosso esquema university, escreva: 
+--Ache os pontos totais recebidos por aluno, para todos os cursos realizados por ele.
+    	 select student.id, student.name, course.title, department.dept_name, course.credits, 
+SELECT student.ID, SUM(course.credits * grade_points.points) AS total_points
+FROM student
+JOIN takes ON student.ID = takes.ID
+JOIN course ON takes.course_id = course.course_id
+JOIN grade_points ON takes.grade = grade_points.grade
+GROUP BY student.ID;
+
